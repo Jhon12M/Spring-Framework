7f48f37dd6956466d01a5c6fba5cd66f4ff122cc
Jonathan Perafan Moreno <jonamore51@gmail.com>
2024-09-26 10:03:40 -0500

    Acceso a datos con JPA

---
+++ gs-accessing-data-jpa-main/initial/src/main/java/com/example/accessingdatajpa/Customer.java
@@ -0,0 +1,34 @@
+package com.example.accessingdatajpa;
+
+public class Customer {
+    
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {}
+
+  public Customer(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "Customer[id=%d, firstName='%s', lastName='%s']",
+        id, firstName, lastName);
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+}
